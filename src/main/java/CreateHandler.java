e72c76fc2930fdd3cb153c0c6a27e4d817aaee37
IshiiTaisei13013 <Taisei_Ishii@hulft.com>
2021-08-05 17:41:58 +0900

    [update] todoを記述

--- src/main/java/CreateHandler.java
+++ src/main/java/CreateHandler.java
@@ -10,45 +10,21 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
+// TODO: 2021/08/05 ファイルを作成する機能に変更する
+// TODO: 2021/08/05 nameとbodyの入れ子構造を受け取れるように
+
 public class CreateHandler extends AbstractS3Crud{
-    //受け取ったKeyでS3上のファイルのKeyを削除する
-    //削除後のファイルをアップロードして更新する。
+    //受け取ったJson形式のファイルで更新を行う。
     @Override
     public String handleRequest(Map<String,Object> event, Context context) {
 
         //Jsonが入れ子になっていても型に合わせて取得できる
         Map<String,String> body = (Map<String, String>) event.get("body");
 
-        //zipcodeのみをリクエスト本文から受け取る
-        String zipcode = body.get("zipcode");
-
         //クライアント認証
         AmazonS3 S3Client = authS3Client();
 
-        //S3からファイルをInputStream型として取得
-        S3Object S3File = S3Client.getObject(S3_BUCKET_NAME, S3_BUCKET_KEY);
-        InputStream S3ObjectStream = S3File.getObjectContent();
-
-        //取得したS3のファイルをJsonに変換
-        JSONObject jsonObject = inputStreamToJSONObject(S3ObjectStream);
-
-        //変換できなかった場合、jsonObjectにはnullが代入される
-        if (jsonObject != null) {
-
-            //Keyで削除(zipcodeで削除する)
-            jsonObject.remove(zipcode);
-        } else {
-            //S3の読み込みが失敗または空の可能性がある。
-            return "jsonObject is null";
-        }
-
-        //JSONObjectをInputStreamに変換
-        String str = jsonObject.toString();
-        InputStream is = new ByteArrayInputStream(str.getBytes(StandardCharsets.UTF_8));
-
-        //metadataを作成
-        ObjectMetadata meta = new ObjectMetadata();
-        meta.setContentLength(str.length());
+        //受け取ったnameとbodyを元にファイルを形成？
 
         //S3にアップロード
         S3Client.putObject(new PutObjectRequest(S3_BUCKET_NAME,S3_BUCKET_KEY,is,meta));
