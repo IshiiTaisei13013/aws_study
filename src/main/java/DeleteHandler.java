382b0afdcd6323294afe74f44777aa0b71388d40
IshiiTaisei13013 <Taisei_Ishii@hulft.com>
2021-08-05 16:24:20 +0900

    [update]  DeleteHandlerを修正

--- src/main/java/DeleteHandler.java
+++ src/main/java/DeleteHandler.java
@@ -11,7 +11,6 @@ import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 public class DeleteHandler extends AbstractS3Crud{
-
     //受け取ったKeyでS3上のファイルのKeyを削除する
     //削除後のファイルをアップロードして更新する。
     @Override
