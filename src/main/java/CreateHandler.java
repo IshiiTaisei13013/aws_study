3f6da05fcdafac2515f1300c709bce8f1972e5ea
IshiiTaisei13013 <Taisei_Ishii@hulft.com>
2021-08-10 12:02:12 +0900

    [update] CreateHandlerを記述

--- src/main/java/CreateHandler.java
+++ src/main/java/CreateHandler.java
@@ -2,25 +2,24 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.amazonaws.services.s3.model.S3Object;
-import org.json.simple.JSONObject;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
-// TODO: 2021/08/05 ファイルを作成する機能に変更する
-
 public class CreateHandler extends AbstractS3Crud{
-    //受け取った名前のファイルを作成する
+    // 受け取った名前と本文を持つファイルを作成する
     // 想定するリクエスト本文の形式 (eventの中身)
     // {
     //      "body":{
     //              "name" : "newFile.json",
+    //              "text" : "hogehoge"
     //              }
     // }
-    //
+
+    // api gatewayがbodyで囲んでくれるので
+    // curl等で呼ぶときは必要なのはnameとtextのみ
 
     @Override
     public String handleRequest(Map<String,Object> event, Context context) {
@@ -32,9 +31,17 @@ public class CreateHandler extends AbstractS3Crud{
         AmazonS3 S3Client = authS3Client();
 
         String name = body.get("name");
+        String text = body.get("text");
+
+        //String to InputStream
+        InputStream is = new ByteArrayInputStream(text.getBytes(StandardCharsets.UTF_8));
+
+        //メタデータを作成
+        ObjectMetadata meta = new ObjectMetadata();
+        meta.setContentLength(text.length());
 
         //S3にアップロード
-        S3Client.putObject(new PutObjectRequest(S3_BUCKET_NAME,name,null,null));
+        S3Client.putObject(new PutObjectRequest(S3_BUCKET_NAME,name,is,meta));
 
         //成功メッセージ
         return "S3 file is created!";
