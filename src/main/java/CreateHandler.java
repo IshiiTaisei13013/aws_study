dd5c924f1d8e8fc740f807bcb934b33271707666
IshiiTaisei13013 <Taisei_Ishii@hulft.com>
2021-08-10 10:56:30 +0900

    [delete] 必要ないファイルを削除

--- src/main/java/CreateHandler.java
+++ src/main/java/CreateHandler.java
@@ -1,7 +1,14 @@
+import com.amazonaws.services.kinesisanalytics.model.Input;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PresignedUrlUploadRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 public class CreateHandler extends AbstractS3Crud{
@@ -24,9 +31,14 @@ public class CreateHandler extends AbstractS3Crud{
         AmazonS3 S3Client = authS3Client();
 
         String name = body.get("name");
+        String text = body.get("text");
+
+        InputStream is = new ByteArrayInputStream(text.getBytes(StandardCharsets.UTF_8));
+        ObjectMetadata meta = new ObjectMetadata();
+        meta.setContentLength(text.length());
 
         //S3にアップロード
-        S3Client.putObject(new PutObjectRequest(S3_BUCKET_NAME,name,null,null));
+        S3Client.putObject(new PutObjectRequest(S3_BUCKET_NAME,name,is,meta));
 
         //成功メッセージ
         return "S3 file is created!";
